5020b9550554df3c42a639be5531ef3839cfd861
Marnel Fresh'eur <gmlginolias@gmail.com>
2021-01-11 00:56:13 +0100

    I've done with the about component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {CurrencyComponent} from './currency/currency.component';
+import {AboutComponent} from './about/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {path: '', component: CurrencyComponent},
+    {path: 'about', component: AboutComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
