faa1318f836dda1c5b2038574aaefbe617085a79
Marnel Fresh'eur <gmlginolias@gmail.com>
2021-01-11 01:19:31 +0100

    Done with a basic converter with static data

--- src/app/currency/currency.component.ts
+++ src/app/currency/currency.component.ts
@@ -3,13 +3,23 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-currency',
   templateUrl: './currency.component.html',
-  styleUrls: ['./currency.component.sass']
+  styleUrls: ['./currency.component.scss']
 })
 export class CurrencyComponent implements OnInit {
+
+  rate = 1.74;
+  from = 'USD';
+  to = 'CAD';
+  price = 2;
+
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  convert() {
+    return this.price * this.rate;
   }
 
 }
